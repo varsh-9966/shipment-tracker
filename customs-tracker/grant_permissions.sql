3cb222ce2d5483212e62d8f242e570ddc01bd9db
Varshini <varshini@twite.ai>
2026-05-12 15:57:27 +0530

    changed

--- customs-tracker/grant_permissions.sql
+++
@@ -1,3 +0,0 @@
--- Grant access to the authenticated and anon roles so the API can access the table
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE public.profiles TO authenticated;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE public.profiles TO anon;
